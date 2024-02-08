3b49db0a542eb96f72a1106a7c166c4c62b66c46
fefei69 <coolpixs04@gmail.com>
2024-02-08 13:20:01 -0800

    pbd works

--- pbd.py
+++ pbd.py
@@ -2,7 +2,7 @@ import time
 import numpy as np
 import pyvista as pv
 import pdb
-
+import os 
 # Should only for 2 positions X1 and X2
 def update_pos(position):
     diff, norm = pos_diff(position)
@@ -34,42 +34,54 @@ spheres = pv.MultiBlock()
 # bounds for x 
 bounds = [0, 100]
 MASS = 1
-DISTANCE = 50
+DISTANCE = 25
 # Generate particle positions
 number_of_particles = 5
 particle_positions = generate_particles(number_of_particles, bounds)
-print(particle_positions)
-update_pos(particle_positions)
+# print(particle_positions)
+# x1_test,x2_test = update_pos(particle_positions)
+# print(x1_test)
 # pdb.set_trace()
 
-
+# camera_focal_point = (-0.097, 0.168, -0.740)
+camera_position =(500, 500, 0)
+# camera_up = (-0.714, -0.288, -0.639)
+num = len(os.listdir('videos'))
 plotter = pv.Plotter()
 pv.set_plot_theme('document')
 plotter.add_axes()
 plotter.add_mesh(spheres, color='red', show_edges=True, lighting=True)
 plotter.show(interactive_update=True)
 plotter.camera_position = 'xz'
+plotter.open_movie(f"videos/distance_constraint_PBD{num}.mp4")
+
+# plotter.camera.focal_point = camera_focal_point
+plotter.camera.position = camera_position
+# plotter.camera.up = camera_up
 # plotter.camera_position = [(25, 25, 25), (5, 5, 5), (0, 0, 1)]
 # Set up the animation
-frames = 1500  # Number of animation frames
+frames = 1000  # Number of animation frames
 dt = 0.01  # Time step for animation
-
+plotter.write_frame()
 for frame in range(frames):
+    start = time.time()
     # Update particle positions based on gravity
     # not updating the first particle
+    # pdb.set_trace()
     particle_positions[1:, 2] -= 0.1 * frame * dt
     for j in range(particle_positions.shape[0]-1):
+        # print(particle_positions[1:,2])
+        # pdb.set_trace()
         for _ in range(10):
-            x1,x2 = update_pos(particle_positions)
+            x1,x2 = update_pos(particle_positions[j:j+2])
             # print(x1)
-            pdb.set_trace()
+            # pdb.set_trace()
             if j == 0:
                 particle_positions[j] = particle_positions[j]
                 particle_positions[j+1] = particle_positions[j+1] - x2[0]
             else:
                 particle_positions[j] = particle_positions[j] - x1[0]
                 particle_positions[j+1] = particle_positions[j+1] - x2[0]
-            # diff, norm = pos_diff(particle_positions)
     # pdb.set_trace()
     # Create a new set of spheres for each frame
     spheres = pv.MultiBlock()
@@ -80,10 +92,10 @@ for frame in range(frames):
     # Add the spheres to the plotter
     plotter.clear()
     plotter.add_mesh(spheres, color='red')
-    
     # plotter.camera_position = 'xy'
     # Render the scene
     plotter.render()
     plotter.update()
-
+    plotter.write_frame()
+    print(f'Frame Time: {time.time() - start}')
 plotter.close()
