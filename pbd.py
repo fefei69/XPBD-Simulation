f151281e16924e2ebe353917e1ab171486e79ff8
fefei69 <coolpixs04@gmail.com>
2024-02-12 17:25:48 -0800

    small update

--- pbd.py
+++ pbd.py
@@ -5,16 +5,19 @@ import pdb
 import os 
 # Should only for 2 positions X1 and X2
 def update_pos(position,L):
+    dt = 0.01
+    compliance = 1e-8/dt/dt
     diff = pos_diff(position)
-    norm = np.linalg.norm(diff, axis=-1, keepdims=True) + 1e-4
+    norm = np.linalg.norm(diff, axis=-1, keepdims=True)
     # computing lambda (for each constraint)
-    L = (norm - DISTANCE)/2
+    delta_L = (-(norm - DISTANCE) - compliance*L)/(2+compliance)
+    L_new = L - delta_L
     n = diff/norm
-    delta_x1 = L * 1/2 * (-n)
-    delta_x2 = L * 1/2 * (n)
-    
+    delta_x1 = delta_L * MASS_INV * (n)
+    delta_x2 = delta_L * MASS_INV * (-n)
     # pdb.set_trace()
-    return delta_x1, delta_x2, delta_L
+    return delta_x1, delta_x2, L_new
+
 
 # Should only for 2 positions X1 and X2
 # X2 - X1
@@ -37,8 +40,8 @@ spheres = pv.MultiBlock()
 ACC = np.array([0.,0.,-9.8])
 # Setting each particle with same mass
 MASS_INV = 1
-L = 0
-save = False
+new_lambda = 0
+save = True
 Velocity = 0
 Fixed_point = True
 # Generate particle positions
@@ -70,27 +73,23 @@ plotter.add_mesh(spheres, color='red', show_edges=True, lighting=True)
 plotter.show(interactive_update=True)
 plotter.camera_position = 'xz'
 if save == True:
-    plotter.open_movie(f"videos/distance_constraint_PBD{num}.mp4")
+    plotter.open_movie(f"videos/distance_constraint_PBD_test{num}.mp4")
     plotter.write_frame()
 plotter.camera.position = camera_position
 
 frames = 400 
 dt = 0.01  # Time step for animation
-
+solver_iter = 100
 for frame in range(frames):
     start = time.time()
     # not updating the first particle
-    # pdb.set_trace()
-    # particle_positions[1:, 2] -= Velocity * frame * dt
     particle_positions[1:] = particle_positions[1:] + V * frame * dt + ACCELARATION * (frame * dt)**2   
     old_particle_positions = particle_positions.copy()
     # print(f"it: {frame}, Before loop\n",particle_positions)
     for j in range(particle_positions.shape[0]-1):
-        # print(particle_positions[1:,2])
         # pdb.set_trace()
-        for _ in range(100):
-            x1,x2, delta_L = update_pos(particle_positions[j:j+2],L)
-            L = L - delta_L
+        for _ in range(solver_iter):
+            x1,x2, new_lambda = update_pos(particle_positions[j:j+2],new_lambda)
             # print(L)
             # print(x1)
             # pdb.set_trace()
@@ -100,12 +99,13 @@ for frame in range(frames):
             else:
                 particle_positions[j] = particle_positions[j] - x1[0]
                 particle_positions[j+1] = particle_positions[j+1] - x2[0]
-    if frame == 20:
-        pdb.set_trace()
-    print(particle_positions[1:])
-    print(old_particle_positions[1:])
-    V = (1/dt)*(particle_positions[1:] - old_particle_positions[1:])
-    print("Velocity",particle_positions[1:] - old_particle_positions[1:])
+    # if frame == 20:
+    #     pdb.set_trace()
+    # print(particle_positions[1:])
+    # print(old_particle_positions[1:])
+    V = (1/dt/frames)*(particle_positions[1:] - old_particle_positions[1:])
+    # print("Velocity",particle_positions[1:] - old_particle_positions[1:])
+    print("Distance",np.linalg.norm(pos_diff(particle_positions[1:]), axis=-1, keepdims=True))
     
     # print(f"it:{frame} , after loop\n",particle_positions)
     # print(old_particle_positions)
@@ -125,5 +125,5 @@ for frame in range(frames):
     plotter.update()
     if save == True:
         plotter.write_frame()
-    print(f'Frame Time: {time.time() - start}')
+    print(f'Frame Time: {time.time() - start} , Frame:{frame}')
 plotter.close()
